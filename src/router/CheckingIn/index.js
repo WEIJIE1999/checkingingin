2ef87f6834911cdcb89d425a91e91a09be79e5eb
weijie <shiweijie@910app.com>
2021-01-26 14:28:22 +0800

    .

--- src/router/CheckingIn/index.js
+++ src/router/CheckingIn/index.js
@@ -5,7 +5,7 @@ export default {
   // route level code-splitting
   // this generates a separate chunk (about.[hash].js) for this route
   // which is lazy-loaded when the route is visited.
-  component: () => import(/* webpackChunkName: "about" */ "@/views/home.vue"),
+  component: () => import(/* webpackChunkName: "about" */ "@/views/Home.vue"),
   children: [
     { path: "user", component: () => import("@/views/CheckingIn/User.vue") },
     {
